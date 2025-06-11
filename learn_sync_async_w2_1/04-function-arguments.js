0f435cf62ad27a0ed63c2c6398dff74c641cd68d
SumitGarg <garg10sumit@gmail.com>
2025-06-11 17:19:53 +0530

    Added higher-order function example to demonstrate passing functions as arguments

--- learn_sync_async_w2_1/04-function-arguments.js
+++ learn_sync_async_w2_1/04-function-arguments.js
@@ -11,5 +11,9 @@ function div(a, b) {
   return a / b;
 }
 console.log(sum(1, 2));
+function doOperation(a, b, op) {
+  return op(a, b);
+}
+console.log(doOperation(1, 2, sum));
 // Demonstrating basic function arguments and return values in JavaScript
 // Each function takes parameters, processes them, and returns the result
---
+++ learn_sync_async_w2_1/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+function doOperation(a,b,op){
+  return op(a,b);
+}
+console.log(doOperation(1,2,sum));
