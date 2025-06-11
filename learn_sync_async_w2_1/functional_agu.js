8abdae2255baffcc0b2b7feeffd27880e52801cc
SumitGarg <garg10sumit@gmail.com>
2025-06-11 17:09:44 +0530

    Added basic arithmetic functions to demonstrate function arguments and return values

---
+++ learn_sync_async_w2_1/functional_agu.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+  return a + b;
+}
+function mul(a, b) {
+  return a * b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function div(a, b) {
+  return a / b;
+}
+console.log(sum(1, 2));
+// Demonstrating basic function arguments and return values in JavaScript
+// Each function takes parameters, processes them, and returns the result
