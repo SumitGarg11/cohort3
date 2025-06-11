09200cf644831c273f6923631741ce06d0195f11
SumitGarg <garg10sumit@gmail.com>
2025-06-11 16:12:50 +0530

    Added simple synchronous example to understand function calls and execution order in JavaScript

--- learn_sync_async_w2_1/index.js
+++ learn_sync_async_w2_1/index.js
@@ -1,7 +1,16 @@
 function sum(a,b){
     return a+b;
 }
-var ans = sum(1,2);
-console.log(ans);
+var result = sum(1,2);
+console.log(result);
+
+function sum2(n){
+    let ans = 0;
+    for(let i = 1; i<=n; i++){
+        ans += i;
+    }
+    return ans;
+}
+console.log(sum2(5));
 // JS executes code line by line (synchronously).
 // Function sum() is called, completes execution, and only then console.log() runs.
